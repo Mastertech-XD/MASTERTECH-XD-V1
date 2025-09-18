e23a3347c04df08a6e1517ab892f5e7cb3159db1
Mastertech-XD <masterpeaceelite@gmail.com>
2025-09-18 18:58:40 +0300

    ✨💎 MASTERPEACE ELITE 💎✨

--- settingss.js
+++ settingss.js
@@ -2,9 +2,9 @@
 module.exports = {
   SESSION_ID: "",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "254743727510", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
-  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "Africa/Nairobi" //put your country timeZone....leave blank if u don't know.
 };
